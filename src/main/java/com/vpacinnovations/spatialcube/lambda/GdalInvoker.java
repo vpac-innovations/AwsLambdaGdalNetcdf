9464a456b9214e2c462fd68b5127c8ca58cedf3b
Lachlan Hurst <lachlan.hurst@gmail.com>
2017-09-06 09:39:02 +0000

    rewrote invoker to make a single async lambda request

--- src/main/java/com/vpacinnovations/spatialcube/lambda/GdalInvoker.java
+++ src/main/java/com/vpacinnovations/spatialcube/lambda/GdalInvoker.java
@@ -2,31 +2,68 @@ package com.vpacinnovations.spatialcube.lambda;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.handlers.AsyncHandler;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.AWSLambdaAsync;
+import com.amazonaws.services.lambda.AWSLambdaAsyncClientBuilder;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.Future;
 
 public class GdalInvoker{
 
-    public static void main(String[] args) {
+    private static class AsyncGdalHandler implements AsyncHandler<InvokeRequest, InvokeResult>
+    {
+        public void onSuccess(InvokeRequest req, InvokeResult res) {
+            System.out.println("\nLambda function returned:");
+            ByteBuffer response_payload = res.getPayload();
+            System.out.println(new String(response_payload.array()));
+            System.exit(0);
+        }
+
+        public void onError(Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws JsonProcessingException{
         AWSCredentialsProvider cp = new EnvironmentVariableCredentialsProvider();
 
-        final GdalService gdalService = LambdaInvokerFactory.builder()
-            .lambdaClient(AWSLambdaClientBuilder.standard().withCredentials(cp).build())
-            .build(GdalService.class);
+        AWSLambdaAsync client = 
+            AWSLambdaAsyncClientBuilder.standard().withCredentials(cp).build();
 
         S3DataRequest input = new S3DataRequest(
             "lambda-geospatial-test-data",
             "road-network/RoadNetwork_tile_x9_y23.nc");
 
-        S3DataResponse output = gdalService.getGdalData(input);
-        if (output.getSuccess()) {
-            System.out.println(
-                String.format("min %s, max %s", 
-                              output.getMinimum(), 
-                              output.getMaximum()));
-        } else {
-            System.out.println("failed to get min/max");
+        ObjectMapper mapper = new ObjectMapper();
+        String inputJson = mapper.writeValueAsString(input);
+
+        InvokeRequest req = new InvokeRequest()
+            .withFunctionName("gdalJava")
+            .withPayload(inputJson);
+
+        AsyncGdalHandler handler = new AsyncGdalHandler();
+        Future<InvokeResult> future_res = client.invokeAsync(req, handler);
+
+        System.out.print("Waiting for async callback");
+        while (!future_res.isDone() && !future_res.isCancelled()) {
+            // perform some other tasks...
+            try {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e) {
+                System.err.println("Thread.sleep() was interrupted!");
+                System.exit(0);
+            }
+            System.out.print(".");
         }
 
     }
--- src/main/java/com/vpacinnovations/spatialcube/lambda/GdalService.java
+++
@@ -1,9 +0,0 @@
-package com.vpacinnovations.spatialcube.lambda;
-
-import com.amazonaws.services.lambda.invoke.LambdaFunction;
-
-public interface GdalService {
-    @LambdaFunction(functionName="gdalJava")
-    S3DataResponse getGdalData(S3DataRequest input);
-}
-
